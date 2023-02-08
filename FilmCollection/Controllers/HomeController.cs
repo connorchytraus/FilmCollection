79b2cded6d35209b93609e37ba49cfd4b13bf6c4
Connor Chytraus <connorchyt@gmail.com>
2023-02-07 18:12:03 -0700

    mid

--- FilmCollection/Controllers/HomeController.cs
+++ FilmCollection/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace FilmCollection.Controllers
             return View();
         }
 
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ FilmCollection/Models/MoviesResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Models
+{
+    public class MoviesResponse
+    {
+        [Required(ErrorMessage ="Category is required")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Year is required")]
+        public Int32 Year { get; set; }
+        [Required(ErrorMessage = "Director is required")]
+        public string Director { get; set; }
+        [Required(ErrorMessage = "Rating is required")]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
